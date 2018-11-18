d0233923345f016560f44a76f2ff77e9e3f36d43
zootrashh <thetrashpits@gmail.com>
2018-11-18 01:31:23 -0800

    adding love

---
+++ commands/love/love.js
@@ -0,0 +1,16 @@
+var loveMsg = "";
+var number = Math.floor(Math.random() * 100) + 1;
+
+if ("$(query)" === "") {
+	loveMsg = "$(user) loves everything. :D";
+}
+
+else if ("$(query)" == "zootrash"||"$(query)" == "@zootrash"||"$(query)" == "zoo") {
+	loveMsg = "$(user) loves zootrash!";
+} 
+
+else {
+	loveMsg = `The love between $(user) and $(query) is ${number}%! PogChamp`;
+} 
+
+loveMsg;
