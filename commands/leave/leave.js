0ef0eec72e7e16d37342df6652fb5b61f3ca0a6b
zootrashh <thetrashpits@gmail.com>
2018-11-18 13:05:38 -0800

    adding leave command

---
+++ commands/leave/leave.js
@@ -0,0 +1,13 @@
+msg = [
+
+	"has left the building.",
+	"has left the chat.",
+	"has left the chatroom.",
+	"has left the lobby.",
+	"gets up and leaves.",
+	"runs away!",
+	"lights a fire and leaves.",
+	"says 'k bye.' and leaves."
+];
+
+msg[Math.floor(Math.random() * msg.length)];
