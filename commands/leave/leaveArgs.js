3473ddb6d3ed138fcc26439de8ad8df212063683
zootrashh <thetrashpits@gmail.com>
2018-11-18 14:16:39 -0800

    made two leave files

---
+++ commands/leave/leaveArgs.js
@@ -0,0 +1,12 @@
+msg = [
+	"then leaves the building.",
+	"then leaves the chat.",
+	"then leaves the chatroom.",
+	"then leaves the lobby.",
+	"then gets up and leaves.",
+	"then runs away!",
+	", lights a fire, then leaves.",
+	"then flees!"
+]
+
+msg[Math.floor(Math.random() * msg.length)];
