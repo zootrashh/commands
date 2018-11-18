cb0e00a5c829f2a55e67f2c9cdc72692d5afb355
zootrashh <thetrashpits@gmail.com>
2018-11-18 12:38:38 -0800

    adding lurk

---
+++ commands/lurk/lurk.js
@@ -0,0 +1,13 @@
+msg =
+[
+	"is now lurking in the shadows...",
+	"hides behind the bushes...",
+	"observes from a distance...",
+	"patrols atop the zoo gates...",
+	"is patrolling the zoo grounds...",
+	"is now guarding the trash pits...",
+	"throws a smoke bomb and disappears!",
+
+];
+
+msg[Math.floor(Math.random() * a.length)];
