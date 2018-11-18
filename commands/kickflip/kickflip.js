cc2d49a49b644974c611cd19da6690f61aae3d4a
zootrashh <thetrashpits@gmail.com>
2018-11-18 03:00:56 -0800

    done testing kickflip

--- commands/kickflip/kickflip.js
+++ commands/kickflip/kickflip.js
@@ -2,12 +2,7 @@ a = [
 	"pops a fat kickflip and lands bolts!", 
 	"pops a steezy kickflip and lands clean!", 
 	"pops a kickflip but missed the landing!", 
-	"pops a kickflip but misses the board entirely, receiving a mad shinner!",
-	"a",
-	"b",
-	"cccccccccccccccccccccccccccc",
-	"dddddddddddddddddddddddddddd",
-	"eeeeeeeeeeeeeeeeeeeeeeeeeeee"
+	"pops a kickflip but misses the board entirely, receiving a mad shinner!"
 ];
 
 a[Math.floor(Math.random() * a.length)];
