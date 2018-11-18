911a224845219ea81cd9cb418ca7db7bb3625b18
zootrashh <thetrashpits@gmail.com>
2018-11-18 01:08:00 -0800

    adding kickflip command

---
+++ commands/kickflip/kickflip.js
@@ -0,0 +1,8 @@
+a = [
+	"pops a fat kickflip and lands bolts!", 
+	"pops a steezy kickflip and lands clean!", 
+	"pops a kickflip but missed the landing!", 
+	"pops a kickflip but misses the board entirely, receiving a mad shinner!"
+];
+
+a[Math.floor(Math.random() * a.length)])
