c41b7483a4de7eb0f62c200597bfed815ff04a11
zootrashh <thetrashpits@gmail.com>
2018-11-18 15:44:44 -0800

    adding fight

--- commands/fight/fight.js
+++ commands/fight/fight.js
@@ -1,5 +1,12 @@
 msg = [
-	""
+	"Won by KO!",
+	"Lost by KO!",
+	"Won by submission!",
+	"Lost by submission!",
+	"Won by DQ!",
+	"Lost by DQ!",
 
-]
+];
+
+msg[Math.floor(Math.random() * msg.length)];
 
