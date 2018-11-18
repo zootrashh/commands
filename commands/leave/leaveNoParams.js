81b44bee881a2626c131de863070a5cdc2cee562
zootrashh <thetrashpits@gmail.com>
2018-11-18 14:10:14 -0800

    making two leave files

---
+++ commands/leave/leaveNoParams.js
@@ -0,0 +1,14 @@
+msg = [
+
+	"has left the building.",
+	"has left the chat.",
+	"has left the chatroom.",
+	"has left the lobby.",
+	"gets up and leaves.",
+	"runs away!",
+	"lights a fire and leaves.",
+	"says 'k bye.' and leaves.",
+	"has fled!"
+];
+
+msg[Math.floor(Math.random() * msg.length)];
