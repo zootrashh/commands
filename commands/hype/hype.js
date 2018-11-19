213ac560ac81762287d895772bdc4eaafda261a6
zootrashh <thetrashpits@gmail.com>
2018-11-18 17:04:50 -0800

    adjusting hype

--- commands/hype/hype.js
+++ commands/hype/hype.js
@@ -1,12 +1,10 @@
 msg = [
-	"PogChamp", 
-	"PokPikachu", 
-	"CurseLit", 
-	"FBtouchdown", 
-	"BibleThump",
-	"ragsGGs",
-	"GivePLZ",
-	"TakeRNG"
+	"PogChamp BibleThump CurseLit", 
+	"CurseLit FBtouchdown BibleThump", 
+	"FBtouchdown GivePLZ TakeNRG", 
+	"BibleThump PogChamp FBtouchdown",
+	"GivePLZ TakeNRG GivePLZ TakeNRG",
+	"TakeNRG GivePLZ TakeNRG GivePLZ"
 ];
 
 msg[Math.floor(Math.random() * msg.length)];
