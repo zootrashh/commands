cc0ef8ec4a8476c17777efff03e4965f1848d70a
zootrashh <thetrashpits@gmail.com>
2018-11-18 16:41:14 -0800

    removing more hype

--- commands/hype/hype.js
+++ commands/hype/hype.js
@@ -1,8 +1,8 @@
 msg = [
-	"MAJOR HYPUUUUU!! PogChamp PokPikachu CurseLit BibleThump PogChamp ragsGGs FBtouchdown GivePLZ TakeNRG PopCorn" ,
-	"HYYYYPPEEE!!! PokPikachu CurseLit BibleThump PogChamp ragsGGs GivePLZ TakeRNG PogChamp ragsGGs CurseLit FBtouchdown", 
-	"LETTSSSS GOOOOO!!! PogChamp PogChamp PogChamp PogChamp PogChamp CurseLit CurseLit TwitchLit PokPikachu", 
-	"YOOOOOOOOOOOOOO!!! PopCorn FBtouchdown PogChamp CurseLit PokPikachu ragsGGs PogChamp PogChamp GivePLZ TakeNRG"
+	"MAJOR HYPUUUUU!! PogChamp PokPikachu CurseLit BibleThump PogChamp ragsGGs", 
+	"HYYYYPPEEE!!! PokPikachu CurseLit BibleThump PogChamp ragsGGs GivePLZ TakeRNG", 
+	"LETTSSSS GOOOOO!!! PogChamp PogChamp PogChamp CurseLit CurseLit TwitchLit PokPikachu", 
+	"YOOOOOOOOOOOOOO!!! FBtouchdown PogChamp CurseLit PokPikachu ragsGGs GivePLZ TakeNRG"
 ];
 
 msg[Math.floor(Math.random() * msg.length)];
