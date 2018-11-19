1b3f047f48fccc440d2d78113db4675b0cafbc5e
zootrashh <thetrashpits@gmail.com>
2018-11-18 17:13:19 -0800

    adjusting hype

--- commands/hype/hype.js
+++ commands/hype/hype.js
@@ -1,8 +1,8 @@
 msg = [
-	"PogChamp BibleThump CurseLit TakeNRG GivePLZ", 
-	"CurseLit FBtouchdown BibleThump CurseLit PogChamp", 
-	"FBtouchdown GivePLZ TakeNRG BibleThump TakeNRG", 
-	"BibleThump PogChamp FBtouchdown PogChamp CurseLit",
+	"PogChamp PogChamp CurseLit TakeNRG GivePLZ", 
+	"CurseLit FBtouchdown PogChamp CurseLit PogChamp", 
+	"FBtouchdown GivePLZ TakeNRG PogChamp TakeNRG", 
+	"PogChamp PogChamp FBtouchdown PogChamp CurseLit",
 	"GivePLZ TakeNRG GivePLZ TakeNRG GivePLZ TakeNRG",
 	"TakeNRG GivePLZ TakeNRG GivePLZ TakeNRG GivePLZ"
 ];
