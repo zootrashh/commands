f57ca9f5779537902cee90f53cd18140ae990ca1
zootrashh <thetrashpits@gmail.com>
2018-11-18 16:24:26 -0800

    added hype

---
+++ commands/hype/hype.js
@@ -0,0 +1,8 @@
+msg = [
+	"MAJOR HYPUUUUU!! PogChamp PokPikachu PogChamp PogChamp CurseLit BibleThump PogChamp ragsGGs ragsGGs FBtouchdown GivePLZ TakeNRG GivePLZ TakeNRG PopCorn PogChamp PogChamp",
+	"HYYYYPPEEE!!! PokPikachu PogChamp CurseLit BibleThump PogChamp ragsGGs GivePLZ TakeRNG PogChamp PogChamp ragsGGs CurseLit CurseLit FBtouchdown", 
+	"LETTSSSS GOOOOO!!! PogChamp PogChamp PogChamp PogChamp PogChamp PogChamp PogChamp CurseLit CurseLit TwitchLit PokPikachu", 
+	"YOOOOOOOOOOOOOO!!! PopCorn PopCorn FBtouchdown PogChamp CurseLit CurseLit CurseLit PokPikachu ragsGGs ragsGGs PogChamp PogChamp GivePLZ TakeNRG"
+];
+
+msg[Math.floor(Math.random() * msg.length)];
