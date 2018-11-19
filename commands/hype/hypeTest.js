d65f6dd369084b51f3e6e08d21b7c60b78c7af35
zootrashh <thetrashpits@gmail.com>
2018-11-18 16:59:03 -0800

    added more hype

--- commands/hype/hypeTest.js
+++ commands/hype/hypeTest.js
@@ -1,12 +1,12 @@
 msg = [
 
-	"PogChamp BibleThump PopCorn CurseLit GivePLZ TakeNRG PogChamp CurseLit",
-	"BibleThump PogChamp CurseLit PogChamp GivePLZ TakeNRG CurseLit PogChamp",
-	"PopCorn FBtouchdown TwitchLit PogChamp CurseLit GivePLZ TakeNRG FBtouchdown",
-	"GivePLZ TakeNRG GivePLZ TakeNRG GivePLZ TakeNRG GivePLZ TakeNRG",
-	"PogChamp PogChamp PogChamp PogChamp PogChamp PogChamp PogChamp PogChamp",
-	"BibleThump BibleThump BibleThump BibleThump BibleThump BibleThump",
-	"FBTouchdown FBTouchdown FBTouchdown FBTouchdown FBTouchdown FBTouchdown"
+	"PogChamp BibleThump PopCorn CurseLit GivePLZ TakeNRG ",
+	"BibleThump PogChamp CurseLit PogChamp GivePLZ TakeNRG ",
+	"PopCorn FBtouchdown TwitchLit PogChamp CurseLit GivePLZ ",
+	"GivePLZ TakeNRG GivePLZ TakeNRG GivePLZ TakeNRG",
+	"PogChamp PogChamp PogChamp PogChamp PogChamp PogChamp",
+	"BibleThump BibleThump BibleThump BibleThump BibleThump",
+	"FBTouchdown FBTouchdown FBTouchdown FBTouchdown FBTouchdown"
 ];
 
 msg[Math.floor(Math.random() * msg.length)];
